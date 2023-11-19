8397005f6c821265bedbc613e723c603aa1a26d2
ExtraDev <scott.birner@gmail.com>
2023-11-19 19:47:22 +0100

    Loooooading

--- src/app/common/news.service.ts
+++ src/app/common/news.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of} from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { News } from './models/news';
 
@@ -8,19 +8,30 @@ import { News } from './models/news';
   providedIn: 'root', // Si pas providedIn, les modifications ne seront repercutiées
 })
 export class NewsService {
+  private news?: News[];
+
   constructor(private http: HttpClient) {}
 
-  getNews$(): Observable<News[]> {
+  getNews$(forceRefresh = false): Observable<News[]> {
+    if(this.news && !forceRefresh) {
+      console.log("cached");
+      return of(this.news);
+    }
+
     return this.http.get<{news: News[]}>('../../assets/news.json').pipe(
-      tap(news => console.log(news)),
+      // tap(news => console.log(news)),
       map(fakeNews => fakeNews.news),
-      tap(news => console.log(news)),
+      // tap(news => console.log(news)),
       map(fakeNews => fakeNews.map(fakeNew => Object.assign(new News(), fakeNew))),
-      tap(news =>console.log(news)),
+      // tap(news =>console.log(news)),
       map(fakeNews => fakeNews.map(fakeNew => {
         fakeNew.headline = fakeNew.headline?.toUpperCase();
         return fakeNew;
-      }))
+      })),
+      tap(fakeNews => {
+        this.news = fakeNews;
+        console.log("TOTO", this.news);
+      })
     );
   }
 }
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,21 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { News } from '../common/models/news';
 import { NewsService } from '../common/news.service';
-import { response } from 'express';
-import { tap } from 'rxjs/operators';
+import { tap, map, delay, debounceTime, throttleTime } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  styleUrls: ['./news.component.scss'],
 })
 export class NewsComponent implements OnInit {
+  private askNews = new Subject<boolean>();
+  public loading = true;
   public news?: News[];
+  public newsObservable$ = this.newsService.getNews$();
+  public formatted$?: Observable<News[]>;
 
-  constructor(private newsService: NewsService) { }
+  constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
-    this.newsService.getNews$().pipe().subscribe(response => this.news = response);
+    // this.loadNews();
+    this.format();
+
+    this.askNews.pipe(
+      tap(() => this.loading = true),
+      tap(() => console.warn('event asked')),
+      delay(1000),
+      tap( () => this.loadNews())
+    ).subscribe((value) => {
+      console.log(!value, new Date().getTime());
+    })
   }
 
+  private format(): void {
+    this.formatted$ = this.newsService.getNews$().pipe(
+      map((fakeNews) =>
+        fakeNews.map(fakeNew => {
+          fakeNew.headline = `${fakeNew.id} : ${fakeNew.headline}`;
+          return fakeNew;
+        })
+      )
+    );
+  }
+
+  public reloadNews(): void {
+    this.loadNews();
+  }
+
+  private loadNews(forceRefresh?:boolean): void {
+    this.newsService
+      .getNews$(forceRefresh)
+      .pipe(
+        tap(() => this.loading = false)
+      )
+      .subscribe((response) => (this.news = response));
+  }
+
+  public reloadNewsForceRefresh(): void{
+    this.askNews.next(true);
+    // this.loadNews(true);
+  }
 }
